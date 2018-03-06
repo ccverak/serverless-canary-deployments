d54191141bef90a5e9e83d974af40753efae8be0
Lily <lilibet.borges@gmail.com>
2018-03-06 23:39:32 +0100

    WIP

---
+++ spec/CanaryDeployments.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const { ServerlessCanaryDeployments } = require('../serverless-plugin-canary-deployments');
+
+describe('CanaryDeployments', () => {
+  describe('ApiGateway support', () => {
+    it('composes the template object for the provided configuration', () => {
+
+    });
+  });
+}); 
+
